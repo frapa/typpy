c5ff236bcc619a5cb234391fb1f0ee466bde0565
Francesco Pasa <francescopasa@gmail.com>
2021-08-25 19:08:13 +0200

    feat: add test for is_subtype and cover more cases

---
+++ tests/test_is_subtype.py
@@ -0,0 +1,72 @@
+from dataclasses import dataclass
+from typing import Type, Tuple, Optional, Union, Any
+
+import pytest
+
+from typpy.is_subtype import is_subtype
+
+
+def type_name(_type: Optional[Type]) -> str:
+    if _type is None:
+        return "None"
+
+    # This if for objects from the typing module,
+    # such as Optional or Union
+    if hasattr(_type, "_name"):
+        return str(_type).replace("typing.", "")
+
+    return _type.__name__
+
+
+@dataclass(frozen=True)
+class IsSubtypeTestCase:
+    actual: Optional[Type]
+    expected: Optional[Type]
+    result: bool
+
+
+@pytest.mark.parametrize(
+    "case",
+    [
+        IsSubtypeTestCase(int, int, True),
+        IsSubtypeTestCase(float, str, False),
+        IsSubtypeTestCase(str, int, False),
+        # Numeric types are a bit of a mess in python
+        # for instance issubclass(int, float) == False,
+        # but type checkers are required to accept
+        # ints where floats are accepted by PEP 484.
+        # This function takes the pragmatic approach
+        # of accepting ints in place of floats.
+        # The same is true for complex numbers.
+        IsSubtypeTestCase(int, float, True),
+        IsSubtypeTestCase(int, complex, True),
+        IsSubtypeTestCase(float, complex, True),
+        # bool is a subclass of int
+        # (the mess above is not enough!)
+        IsSubtypeTestCase(bool, int, True),
+        IsSubtypeTestCase(bool, float, True),
+        IsSubtypeTestCase(bool, complex, True),
+        # None means type(None) and should be accepted
+        IsSubtypeTestCase(None, None, True),
+        # Any should always return True
+        IsSubtypeTestCase(None, Any, True),
+        IsSubtypeTestCase(int, Any, True),
+        IsSubtypeTestCase(Tuple[int, float], Any, True),
+        IsSubtypeTestCase(Any, Any, True),
+        # Tuples
+        IsSubtypeTestCase(Tuple[int, float], Tuple[int, float], True),
+        IsSubtypeTestCase(Tuple[int, str], Tuple[int, float], False),
+        # Union types
+        IsSubtypeTestCase(None, Optional[int], True),
+        IsSubtypeTestCase(int, Optional[int], True),
+        IsSubtypeTestCase(int, Union[int, float, bytes], True),
+        IsSubtypeTestCase(str, Union[int, float, bytes], False),
+        IsSubtypeTestCase(Union[float, bytes], Union[int, float, bytes], True),
+        IsSubtypeTestCase(Union[str, bytes], Union[int, float, bytes], False),
+        # Since is a subclass if float, it should be accepted
+        IsSubtypeTestCase(bool, Union[float, bytes], True),
+    ],
+    ids=lambda case: f"{type_name(case.actual)}-{type_name(case.expected)}-{case.result}",
+)
+def test_is_subtype(case: IsSubtypeTestCase):
+    assert is_subtype(case.actual, case.expected) == case.result
--- typpy/is_subtype.py
+++ typpy/is_subtype.py
@@ -1,21 +1,51 @@
-from typing import Type, Union, Tuple
+from typing import Type, Union, Tuple, Any, Optional
+from inspect import isclass
 
 
 # TODO: test
-def is_subtype(act_type: Type, exp_type: Type) -> bool:
-    origin_type = getattr(exp_type, "__origin__", None)
-    if origin_type is not None:
-        if origin_type == Union:
+def is_subtype(act_type: Optional[Type], exp_type: Optional[Type]) -> bool:
+    act_type = type(None) if act_type is None else act_type
+    exp_type = type(None) if exp_type is None else exp_type
+
+    # optimization for common use case
+    if act_type is exp_type:
+        return True
+
+    # SPECIAL CASES
+    # =============
+    # if the expected type is Any, the result is always True
+    if exp_type is Any:
+        return True
+    # For numeric types, we follow PEP 484
+    elif exp_type is float and act_type in (int, bool):
+        return True
+    elif exp_type is complex and act_type in (float, int, bool):
+        return True
+
+    exp_origin_type = getattr(exp_type, "__origin__", None)
+    if exp_origin_type is not None:
+        # This if for objects from the typing module,
+        # such as Optional or Union
+        if exp_origin_type is Union:
             return _check_union(act_type, exp_type)
-        # elif origin_type == tuple:
-        #     return _check_tuple(act_type, exp_type)
-    else:
+        elif exp_origin_type is Tuple:
+            return _check_union(act_type, exp_t)
+    elif isclass(act_type):
         return issubclass(act_type, exp_type)
+    else:
+        if issubclass(act_type, Tuple):
+            return _check_tuple(act_type, exp_type)
 
     raise NotImplementedError(f"is_subtype() for type {exp_type} is not implemented")
 
 
 def _check_union(act_type: Type, exp_type: Type) -> bool:
+    # If the act_type is an Union, check that act_type is a subset
+    if getattr(act_type, "__origin__", None) is Union:
+        union_types = set(exp_type.__args__)
+        return all(t in union_types for t in act_type.__args__)
+
+    # Otherwise, check it act_type is contained in the Union
     for union_type in exp_type.__args__:
         if is_subtype(act_type, union_type):
             return True
@@ -23,9 +53,9 @@ def _check_union(act_type: Type, exp_type: Type) -> bool:
     return False
 
 
-# def _check_tuple(act_type: Type, exp_type: Type) -> bool:
-#     print(888)
-#     for tuple_type in exp_type.__args__:
-#         print("-->", tuple_type)
-#
-#     return False
+def _check_tuple(act_type: Type, exp_type: Type) -> bool:
+
+    for tuple_type in exp_type.__args__:
+        print("-->", tuple_type)
+
+    return False
